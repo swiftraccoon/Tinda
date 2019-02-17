7a0b15f0b6109283d31d9e94509828f96dba8a92
Swift Raccoon <47705353+swiftraccoon@users.noreply.github.com>
2019-02-17 01:02:36 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Tinda
+
+This was developed with the use of the following repos:
+* https://github.com/bbsmithy/tinder-automation/
+* https://github.com/philipperemy/Deep-Learning-Tinder
+* https://github.com/fbessez/Tinder
+
+# Examples
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='Tinda',
+      version='2019.2',
+      description='Tinder API for Python',
+      long_description=open('README.md').read().strip(),
+      author='Swift Raccoon',
+      #author_email='',
+      url='https://github.com/swiftraccoon/Tinda',
+      py_modules=['tinder_api'],
+      install_requires=['requests',
+                        'robobrowser',
+                        'lxml'],
+      license='MIT License',
+      zip_safe=False,
+      keywords=['tinder-api', 'tinder', 'python-3', 'robobrowser'],
+      #classifiers=[]
+     )
